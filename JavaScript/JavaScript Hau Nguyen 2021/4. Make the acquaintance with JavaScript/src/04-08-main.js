52dbd038175a3466b04715e137b89ee448248cb7
duynghiadev <duynghia22302@gmail.com>
2024-01-16 08:18:03 +0700

    8.1 04-08-variables

---
+++ JavaScript/JavaScript Hau Nguyen 2021/4. Make the acquaintance with JavaScript/src/04-08-main.js
@@ -0,0 +1,10 @@
+// ES5
+var name = 'duynghiadev';
+
+// ES6
+const PI = 3.14;
+const name = 'easy front end';
+name = 'Fronend'; // error Assignment to constant variable.
+
+let count = 1;
+count = 2; // work
