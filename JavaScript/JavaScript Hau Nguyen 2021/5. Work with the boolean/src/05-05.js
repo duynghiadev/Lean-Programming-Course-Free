5f655a9a68ace366864b71595cebb79c001d2e7e
duynghiadev <duynghia22302@gmail.com>
2024-01-21 18:04:26 +0700

    5.1 05-05-comparison

---
+++ JavaScript/JavaScript Hau Nguyen 2021/5. Work with the boolean/src/05-05.js
@@ -0,0 +1,8 @@
+console.log(2 > '1');
+console.log(3 == '3');
+console.log(4 === '4');
+console.log(4 === '4');
+console.log(null == undefined);
+console.log(null == 0);
+console.log(null >= 0);
+console.log('' == 0);
