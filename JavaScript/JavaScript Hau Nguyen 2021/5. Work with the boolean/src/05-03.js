3f0599629bc04db48158b45e9db00b92752e4433
duynghiadev <duynghia22302@gmail.com>
2024-01-21 15:53:31 +0700

    3.1 05-03-truthy-falsy

---
+++ JavaScript/JavaScript Hau Nguyen 2021/5. Work with the boolean/src/05-03.js
@@ -0,0 +1,12 @@
+/**
+ * Truthy vs Falsy
+ *
+ * - Truthy: là giá trị khi chuyển đổi về boolean thì sẽ được giá trị true ✅
+ * - Falsy: là những giá trị khi chuyển đổi về boolean thì sẽ được giá trị false ❌
+ */
+
+/**
+ * Dưới đây là những giá trị thuộc kiểu Falsy:
+ *
+ * => false, 0, -0, 0n, "", '', ``, null, undefined, NaN, document.all
+ */
