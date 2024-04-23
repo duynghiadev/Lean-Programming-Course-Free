e3d0844b1c50b2a2fd1aa0e3ca43bc49622c4d73
duynghiadev <duynghia22302@gmail.com>
2024-04-23 16:46:03 +0700

    13. Say hello bằng nhiều ngôn ngữ

---
+++ JavaScript/JavaScript Hau Nguyen 2021/5. Work with the boolean/src/Bai Tap/05-13.js
@@ -0,0 +1,11 @@
+// 13. Say hello bằng nhiều ngôn ngữ
+
+// Addition of logging "Say hello" in multiple languages
+
+function sayHelloInDifferentLanguages() {
+  console.log('Say hello'); // English
+  console.log('Xin chào'); // Vietnamese
+  console.log('こんにちは'); // Japanese
+  console.log('Hallo'); // Dutch -> tiếng Hà Lan
+}
+sayHelloInDifferentLanguages();
