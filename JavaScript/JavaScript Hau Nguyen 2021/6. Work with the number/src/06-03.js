b7e34c133e106bed0d62223aee9477ef775b88f1
duynghiadev <duynghia22302@gmail.com>
2024-04-24 17:35:05 +0700

    3. Built-in object Math

---
+++ JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/06-03.js
@@ -0,0 +1,8 @@
+console.log('Math.abs:', Math.abs(-5));
+console.log('Math.abs:', Math.abs(5));
+
+console.log('Math.pow:', Math.pow(2, 3));
+console.log('Math.pow:', Math.pow(10, 2));
+
+console.log('Math.sqrt:', Math.sqrt(16));
+console.log('Math.sqrt:', Math.sqrt(81));
