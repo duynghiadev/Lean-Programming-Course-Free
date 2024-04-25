7697a35a727c5758ec774f25403ecdffc9de61db
duynghiadev <duynghia22302@gmail.com>
2024-04-25 17:29:54 +0700

    13. Tìm chữ số lớn nhất của một số nguyên dương

--- JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-13.js
+++ JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-13.js
@@ -1,1 +1,22 @@
 // 13. Tìm chữ số lớn nhất của một số nguyên dương
+
+function findLargestDigit(n) {
+  const numberString = n.toString();
+  console.log('numberString:', numberString, '👉 typeof numberString:', typeof numberString);
+
+  let largestDigit = 0;
+
+  for (let i = 0; i < numberString.length; i++) {
+    const digit = parseInt(numberString[i]);
+    console.log('digit:', digit, '👉 typeof digit:', typeof digit);
+
+    if (digit > largestDigit) {
+      largestDigit = digit;
+    }
+  }
+  return largestDigit;
+}
+
+const number = 874235;
+const largestDigit = findLargestDigit(number);
+console.log('The largest digit in', number, 'is', largestDigit);
