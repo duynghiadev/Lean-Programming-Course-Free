c3506adf8785f770a3af341f5e1e8a0d02cfac7f
duynghiadev <duynghia22302@gmail.com>
2024-04-25 17:09:20 +0700

    12. Bài toán gọi taxi

--- JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-12.js
+++ JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-12.js
@@ -1,1 +1,15 @@
 // 12. Bài toán gọi taxi
+// Các bài mà không có video thì mình tham khảo trên chat-gpt
+
+function calculateTaxiFare(distance) {
+  const baseFare = 2;
+  const ratePerKilometer = 1.5;
+
+  const totalFare = baseFare + ratePerKilometer * distance;
+  return totalFare;
+}
+
+const distanceTraveled = 10;
+const fare = calculateTaxiFare(distanceTraveled);
+
+console.log('The fare for the taxi ride is $' + fare.toFixed(2));
