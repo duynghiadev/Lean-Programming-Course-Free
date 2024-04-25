3792437c33bb21173296ea170ed19cd22ac48658
duynghiadev <duynghia22302@gmail.com>
2024-04-25 13:11:23 +0700

    5. Rounding - Làm tròn số

---
+++ JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/06-05.js
@@ -0,0 +1,4 @@
+console.log('Math.ceil(3.2):', Math.ceil(3.2));
+console.log('Math.floor(2.5):', Math.floor(2.5));
+console.log('Math.round(-1.1):', Math.round(-1.1));
+console.log('Math.trunc(-1.6):', Math.trunc(-1.6));
