958fe97559e5d9073fd2b6299954c472e63ae5d0
duynghiadev <duynghia22302@gmail.com>
2024-04-27 15:11:40 +0700

    4. Lấy substring như thế nào

---
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/07-04.js
@@ -0,0 +1,17 @@
+// slice
+console.log("'Easy Frontend'.slice(0, 4):", 'Easy Frontend'.slice(0, 4));
+console.log("'Easy Frontend'.slice(2):", 'Easy Frontend'.slice(2));
+
+console.log("'Easy Frontend'.slice(-3):", 'Easy Frontend'.slice(-3));
+console.log("'Easy Frontend'.slice(-3, -1):", 'Easy Frontend'.slice(-3, -1));
+
+// substring
+console.log("'Easy Frontend'.substring(0, 4):", 'Easy Frontend'.substring(0, 4));
+console.log("'Easy Frontend'.substring(2):", 'Easy Frontend'.substring(2));
+
+console.log("'Easy Frontend'.substring(-3):", 'Easy Frontend'.substring(-3));
+console.log("'Easy Frontend'.substring(-3, -1):", 'Easy Frontend'.substring(-3, -1));
+
+// slice and substring
+'Easy Frontend'.slice(4, 0);
+console.log("'Easy Frontend'.substring(4, 0):", 'Easy Frontend'.substring(4, 0));
