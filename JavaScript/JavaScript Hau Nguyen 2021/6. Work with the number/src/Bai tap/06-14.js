938a30d8cc8e3f5cec2f59d414cb9825224619ef
duynghiadev <duynghia22302@gmail.com>
2024-04-27 09:24:45 +0700

    14. So sánh 2 số nguyên

--- JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-14.js
+++ JavaScript/JavaScript Hau Nguyen 2021/6. Work with the number/src/Bai tap/06-14.js
@@ -1,1 +1,14 @@
 // 14. So sánh 2 số nguyên
+function compareNumbers(num1, num2) {
+  if (num1 === num2) {
+    return `${num1} is equal to ${num2}`;
+  } else if (num1 > num2) {
+    return `${num1} is greater than ${num2}`;
+  } else {
+    return `${num1} is less than ${num2}`;
+  }
+}
+
+const number1 = 10;
+const number2 = 5;
+console.log(compareNumbers(number1, number2));
