4ad0ea9a1956a8e3a97db2e92fb44689ab83d10d
duynghiadev <duynghia22302@gmail.com>
2024-04-29 15:14:25 +0700

    16. Truy tìm mật mã

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.16.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.16.js
@@ -1,1 +1,13 @@
 // 16. Truy tìm mật mã
+function findSecretCode(text, secretCode) {
+  const pattern = new RegExp(secretCode, 'gi');
+  const matches = text.match(pattern);
+
+  return matches || [];
+}
+
+const text = 'The secret code is ABC123. The ABC123 code is hidden';
+const secretCode = 'ABC123';
+
+const foundOccurrences = findSecretCode(text, secretCode);
+console.log(foundOccurrences);
