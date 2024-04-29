d7918e8ccb8d3a5006b694e26877831cb2df72a4
duynghiadev <duynghia22302@gmail.com>
2024-04-29 14:28:08 +0700

    13. Convert số giây sang chuỗi hhmmss

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.13.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.13.js
@@ -1,1 +1,20 @@
 // 13. Convert số giây sang chuỗi hhmmss
+function secondsToHHMMSS(seconds) {
+  seconds = parseInt(seconds);
+
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+
+  return formattedTime;
+}
+
+const seconds = 3661;
+const formattedTime = secondsToHHMMSS(seconds);
+console.log('formattedTime:', formattedTime);
