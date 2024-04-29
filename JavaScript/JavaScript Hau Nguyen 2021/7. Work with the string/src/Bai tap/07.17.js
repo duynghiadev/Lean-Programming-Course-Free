5db19fce61f62268218436f14fcf7e15b32277f3
duynghiadev <duynghia22302@gmail.com>
2024-04-29 15:47:38 +0700

    17. Trả về full name khi biết first và last name

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.17.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.17.js
@@ -1,1 +1,13 @@
 // 17. Trả về full name khi biết first và last name
+function getFullName(firstName, lastName) {
+  // Using the concat method --> recommend
+  // const fullName = firstName.concat(' ', lastName);
+
+  // Using the '+' operator
+  const fullName = firstName + ' ' + lastName;
+  return fullName;
+}
+
+const firstName = 'John';
+const lastName = 'Doe';
+console.log(getFullName(firstName, lastName));
