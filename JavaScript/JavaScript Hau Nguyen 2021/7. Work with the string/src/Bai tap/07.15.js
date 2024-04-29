afd24279551c010b33c9d06716d7f3ab56806987
duynghiadev <duynghia22302@gmail.com>
2024-04-29 14:58:33 +0700

    15. Rút trích domain từ địa chỉ email

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.15.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.15.js
@@ -1,1 +1,17 @@
 // 15. Rút trích domain từ địa chỉ email
+function extractDomainFromEmail(email) {
+  const domainRegex = /@([^\s@]+)$/;
+  const match = email.match(domainRegex);
+  console.log('match:', match);
+  console.log('match[0]:', match[0]);
+  console.log('match[1]:', match[1]);
+
+  return match ? match[1] : null;
+}
+
+const email1 = 'example@gmail.com';
+const email2 = 'abc@gmail.co.uk';
+
+console.log('extractDomainFromEmail(email1):', extractDomainFromEmail(email1));
+console.log('='.repeat(30));
+console.log('extractDomainFromEmail(email2):', extractDomainFromEmail(email2));
