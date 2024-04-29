b30a25ff22ae436e22e66e786e62d538d15ce8dc
duynghiadev <duynghia22302@gmail.com>
2024-04-29 14:38:33 +0700

    14. Kiểm tra URL có sử dụng phương thức bảo mật

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.14.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.14.js
@@ -1,1 +1,11 @@
 // 14. Kiểm tra URL có sử dụng phương thức bảo mật
+function isSecureUrl(url) {
+  return url.startsWith('https://');
+}
+
+const url1 = 'http://www.example.com';
+const url2 = 'https://www.example.com';
+const url3 = 'https://www.google.com';
+console.log('isSecureUrl(url1):', isSecureUrl(url1));
+console.log('isSecureUrl(url2):', isSecureUrl(url2));
+console.log('isSecureUrl(url3):', isSecureUrl(url3));
