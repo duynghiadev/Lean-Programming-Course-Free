ddad30dfa062ef7d68efab212d6b9cd705807fc1
duynghiadev <duynghia22302@gmail.com>
2024-04-29 14:11:18 +0700

    12. Format số giây luôn hiển thị 2 chữ số

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.12.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.12.js
@@ -1,1 +1,17 @@
 // 12. Format số giây luôn hiển thị 2 chữ số
+function formatSeconds(seconds) {
+  seconds = parseInt(seconds);
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  const formattedTime = `${formattedMinutes}:${formattedSeconds}`;
+  return formattedTime;
+}
+
+const seconds = 75;
+const formattedTime = formatSeconds(seconds);
+console.log('formattedTime:', formattedTime);
