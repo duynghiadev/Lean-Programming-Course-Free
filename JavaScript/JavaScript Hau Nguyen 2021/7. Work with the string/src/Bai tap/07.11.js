1e5c0e12fa5758e26aa6d05473535329859781c9
duynghiadev <duynghia22302@gmail.com>
2024-04-29 13:52:39 +0700

    11. Tìm và xoá các nguyên âm trong câu văn

--- JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.11.js
+++ JavaScript/JavaScript Hau Nguyen 2021/7. Work with the string/src/Bai tap/07.11.js
@@ -1,1 +1,19 @@
 // 11. Tìm và xoá các nguyên âm trong câu văn
+function removeVowels(sentence) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+  const characters = sentence.split('');
+  console.log('characters:', characters);
+
+  const withoutVowels = characters.filter((char) => {
+    console.log('char:', char);
+    return !vowels.includes(char);
+  });
+
+  const result = withoutVowels.join('');
+  return result;
+}
+
+const sentence = 'Tìm và xóa các nguyên âm trong câu văn';
+const result = removeVowels(sentence);
+console.log('result:', result);
