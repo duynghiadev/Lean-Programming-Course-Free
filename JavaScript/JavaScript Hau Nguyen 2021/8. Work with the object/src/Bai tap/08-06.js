45ac455619cb28a5cff0754193bd0577806934ab
duynghiadev <duynghia22302@gmail.com>
2024-05-08 18:39:29 +0700

    6. Viết hàm clone object nhưng không dùng spread operator

--- JavaScript/JavaScript Hau Nguyen 2021/8. Work with the object/src/Bai tap/08-06.js
+++ JavaScript/JavaScript Hau Nguyen 2021/8. Work with the object/src/Bai tap/08-06.js
@@ -1,1 +1,30 @@
 // 6. Viết hàm clone object nhưng không dùng spread operator
+
+function cloneObj(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  let clonedObj = {};
+
+  for (let key in obj) {
+    let typeObj = typeof obj[key] === 'object' && obj[key] !== null;
+
+    if (obj.hasOwnProperty(key)) {
+      clonedObj[key] = typeObj ? cloneObj(obj[key]) : obj[key];
+    }
+  }
+  return clonedObj;
+}
+
+const originalObj = {
+  name: 'John',
+  age: 22,
+  address: {
+    city: 'New York',
+    country: 'USA',
+  },
+};
+
+const resultClonedObj = cloneObj(originalObj);
+console.log('resultClonedObj:', resultClonedObj);
