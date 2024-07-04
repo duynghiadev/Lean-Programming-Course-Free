377c2db80efaadbbfb1a9ab197e69a4d32e47463
duynghiadev <duynghia22302@gmail.com>
2024-07-04 10:03:01 +0700

    9 Các kiểu xuất dữ liệu

---
+++ Python/Playlist1/XuatDuLieu.py
@@ -0,0 +1,7 @@
+print("-" * 20)
+print("{0}x^2+{1}x+{2}=0".format(5, 2, 3))
+
+print("{0:>11}".format("Obama"))
+print("{0:>11}".format("Putin"))
+print("{0:>11}".format("Donal Trump"))
+print("{0:>11}".format("Kim"))
