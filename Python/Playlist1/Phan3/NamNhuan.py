2e731d5e2fbab0c7e3bd38ccf154d9db3309513a
duynghiadev <duynghia22302@gmail.com>
2024-07-04 17:11:20 +0700

    22 Bài tập rèn luyện-Kiểm tra năm nhuần

---
+++ Python/Playlist1/Phan3/NamNhuan.py
@@ -0,0 +1,8 @@
+print("📆 Chương trình kiểm tra năm nhuần 📆")
+
+year = int(input("Mời bạn nhập vào 1 năm: "))
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print("Năm ", year, " là năm nhuần")
+else:
+    print("Năm ", year, " không nhuần")
