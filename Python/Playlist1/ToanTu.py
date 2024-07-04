8f3b157d9558ef366573a058d8e0a951083752e7
duynghiadev <duynghia22302@gmail.com>
2024-07-04 09:29:21 +0700

    7 Các toán tử thường dùng trong Python

---
+++ Python/Playlist1/ToanTu.py
@@ -0,0 +1,27 @@
+x = 5
+y = x**2
+print(x)
+print(y)
+
+year = 2024
+t = year % 2 == 0 and year % 3 > 1
+print(t)
+
+valueYear_1 = year % 2
+print("valueYear_1:", valueYear_1)
+
+valueYear_2 = year % 3
+print("valueYear_2:", valueYear_2)
+
+print("*" * 20)
+print(5 is 5)
+print(5 is not 5)
+
+print("*" * 20)
+print("9 / 2:", 9 / 2)  # 4.5
+
+print("*" * 20)
+print("9 // 2:", 9 // 2)  # 4
+
+print("*" * 20)
+print("9 % 2:", 9 % 2)  # 1
