2eb2b3d95e034f882dfaa681af87cec0cf490783
duynghiadev <duynghia22302@gmail.com>
2024-07-04 13:53:06 +0700

    13 Bài tập rèn luyện-Tính điểm trung bình

---
+++ Python/Playlist1/TinhDiem.py
@@ -0,0 +1,8 @@
+toan = float(input("Nhập điểm toán:"))
+ly = float(input("Nhập điểm lý: "))
+hoa = float(input("Nhập điểm hóa:"))
+
+dtb = (toan + ly + hoa) / 3
+
+print("Điểm trung bình kiểu dài =", dtb)
+print("Điểm trung bình kiểu ngắn =", round(dtb, 2))
