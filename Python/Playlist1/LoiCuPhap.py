7d5de6262ac69f31f6b5cfabc8f80171ba89cd51
duynghiadev <duynghia22302@gmail.com>
2024-07-04 10:17:28 +0700

    10 Các loại lỗi trong Python

---
+++ Python/Playlist1/LoiCuPhap.py
@@ -0,0 +1,11 @@
+y = 2
+x = y + 2
+
+# đây là lỗi cú pháp (syntax)
+# y+2=x
+
+"""
+Lỗi cú pháp: lỗi này thường gặp cho những bạn mới học lập trình
+Lỗi nghiệp vụ: có nghĩa là lỗi mình làm sai yêu cầu tử leader (lỗi này rất là nguy hiểm nhá, nên cẩn thận!)
+Lỗi runtime: có nghĩa là lỗi khi chạy và làm ứng dụng dừng lun
+"""
