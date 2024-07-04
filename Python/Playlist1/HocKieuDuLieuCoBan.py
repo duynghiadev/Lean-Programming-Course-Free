ebd26af3e6e1e026a543628b7d2e32712c984d12
duynghiadev <duynghia22302@gmail.com>
2024-07-04 09:12:30 +0700

    6 Cách ghi chú lệnh trong Python

--- Python/Playlist1/HocKieuDuLieuCoBan.py
+++ Python/Playlist1/HocKieuDuLieuCoBan.py
@@ -1,9 +1,15 @@
+# gán 5 là kiểu int cho biến x
 x = 5
 print(type(x))
 x = 5.5
 print(type(x))
 x = "Obama! happy new year"
 print(type(x))
+
+"""
+Đây là hàm khai báo số phức
+ngoài ra nó cũng tương đương: x = 3 + 2j
+"""
 x = complex(3, 2)
 print(type(x))
 print("Thực = ", x.real, " ,Ảo = ", x.imag)
