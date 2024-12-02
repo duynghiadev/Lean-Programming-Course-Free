67b9a5b0e845af47ab08d700b450339d7ab2fc1a
duynghiadev <duynghia22302@gmail.com>
2024-12-02 16:48:39 +0700

    16.Giải thuật tìm kiếm tuyến tính trên mảng cơ sở

---
+++ Cau truc du lieu va giai thuat/phan 3 - cac giai thuat tim kiem/video16/bai1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void NhapMang(int M[], int n);
+void XuatMang(int M[], int n);
+int LinearSearch(int M[], int n, int x);
+
+int main(int argc, char **argv)
+{
+  int n = 7;
+  int M[n];
+  cout << "Mảng sau khi nhập ngẫu nhiên là: \n";
+  XuatMang(M, n);
+
+  int x;
+  cout << "\nMời bạn nhập vào 1 số: ";
+  cin >> x;
+  int kq = LinearSearch(M, n, x);
+  if (kq == -1)
+
+    cout << "Không thấy " << x << " trong mảng";
+  else
+    cout << "Thấy " << x << " tại vị trí " << kq;
+
+  return 0;
+}
+
+void NhapMang(int M[], int n)
+{
+  srand(time(NULL));
+  for (int i = 0; i < n; i++)
+  {
+    M[i] = rand() % 100;
+  }
+}
+
+void XuatMang(int M[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << M[i] << "\t";
+  }
+}
+
+int LinearSearch(int M[], int n, int x)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (M[i] == x)
+      return i;
+  }
+  return -1;
+}
