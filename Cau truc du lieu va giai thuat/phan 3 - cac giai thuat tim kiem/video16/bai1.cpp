078efb1bc6516685c891bf9e550d566b4f6b3c37
duynghiadev <duynghia22302@gmail.com>
2025-01-24 23:54:30 +0700

    format code

--- Cau truc du lieu va giai thuat/phan 3 - cac giai thuat tim kiem/video16/bai1.cpp
+++ Cau truc du lieu va giai thuat/phan 3 - cac giai thuat tim kiem/video16/bai1.cpp
@@ -19,18 +19,18 @@ int main(int argc, char **argv)
   cout << "\nMời bạn nhập vào 1 số: ";
   cin >> x;
   int kq = LinearSearch(M, n, x);
-  if (kq == -1)
 
+  if (kq == -1)
     cout << "Không thấy " << x << " trong mảng";
   else
     cout << "Thấy " << x << " tại vị trí " << kq;
-
   return 0;
 }
 
 void NhapMang(int M[], int n)
 {
   srand(time(NULL));
+
   for (int i = 0; i < n; i++)
   {
     M[i] = rand() % 100;
